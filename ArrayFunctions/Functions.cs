b4a221a28c49642df43162674ead518f637f9f85
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-15 09:54:25 -0300

    ArrayFunctions created with IsSorted and PrintArray Functions

---
+++ ArrayFunctions/Functions.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace SortingAlgorithms.ArrayFunctions
+{
+    internal class Functions
+    {
+
+        internal static void PrintArray(int[] array)
+        {
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        internal static Boolean IsSorted(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
