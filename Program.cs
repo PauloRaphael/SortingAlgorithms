1eaa75f9d2000a493620c8c1491f17460b274523
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-15 09:54:56 -0300

    QuickSort Test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SortingAlgorithms.ArrayFunctions;
+using SortingAlgorithms.ComparisonBased;
+
+int[] array = [8, 2, 5, 3, 23, 54, 5, 55, 1000, 344, 9, 4, 7, 6, 1, 15];
+
+QuickSort.Sort(array, 0, array.Length - 1);
+
+Functions.PrintArray(array);
