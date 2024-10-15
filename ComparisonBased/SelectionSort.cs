aaf9529b8ee092ec419edae2365b7d76fb1d07f9
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-15 09:44:40 -0300

    Selection Sort Implemented

---
+++ ComparisonBased/SelectionSort.cs
@@ -0,0 +1,22 @@
+﻿namespace SortingAlgorithms.ComparisonBased
+{
+    internal class SelectionSort
+    {
+        internal static void Sort(int[] array)
+        {
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+                (array[i], array[min]) = (array[min], array[i]);
+            }
+        }
+    }
+}
